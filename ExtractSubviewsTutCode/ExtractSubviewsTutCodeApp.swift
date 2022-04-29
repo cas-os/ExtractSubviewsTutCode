db2e3b3bb19c5c450b31c1707a530575f00f606e
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-04-29 11:16:33 +0200

    Initial Commit

---
+++ ExtractSubviewsTutCode/ExtractSubviewsTutCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExtractSubviewsTutCodeApp.swift
+//  ExtractSubviewsTutCode
+//
+//  Created by HAL-9001 on 29/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExtractSubviewsTutCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
